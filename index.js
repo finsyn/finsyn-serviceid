e28446adbbf5108d7f069829cfb01aa7f494394f
Tor Nilsson Öhrn <tornilssonohrn@gmail.com>
2019-04-24 21:29:41 +0200

    feat: isType method

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const getParts = serviceId => serviceId.split('_')
 const getType = serviceId => getParts(serviceId)[0]
 const getId = serviceId => getParts(serviceId)[1]
+const isType = type => serviceId => getType(serviceId) === type
 
 module.exports = {
   getType,
-  getId
+  getId,
+  isType
 }
--- index.spec.js
+++ index.spec.js
@@ -1,5 +1,7 @@
 const t = require('tap')
-const { getType, getId } = require('./index')
+const { getType, getId, isType } = require('./index')
 
 t.equals(getType('test_123'), 'test')
 t.equals(getId('test_123'), '123')
+t.ok(isType('test')('test_123'))
+t.notOk(isType('not-test')('test_123'))
